a2f01b8ccd7c40fb931649f05b1205ebdc2daae2
kirkby <kirkby@neighbor.com>
2020-12-01 19:17:03 -0700

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -7,5 +7,7 @@ jest.mock("./api/fetchMissions");
 console.log(mockFetchMissions);
 
 test("App fetches missions data and render data", () => {
+  mockFetchMissions.mockResolvedValueOnce({ data: [] });
 
+  render(<App />)
 }
