d77cdb9a26e7f9b3c2788eac89a3701cff78df5a
kirkby <kirkby@neighbor.com>
2020-12-01 21:08:00 -0700

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { fetchMissions as mockFetchMission } from './api/fetchMissions';
+
+// mock the api so we don't make a call and prep to be told what value to return
+jest.mock()
 
 test("App renders", () => {
+  render(<App />);
+});
+
+test("App fetches and renders missions data", () => {
+  // tell the test system what we want the mission data to return
+
   render(<App />);
 })
