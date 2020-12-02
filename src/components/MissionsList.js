fbc65bb87fa3833cf8aa957f07057074a0fe5e6c
kirkby <kirkby@neighbor.com>
2020-12-01 20:51:01 -0700

    lecture work-in-progress

--- src/components/MissionsList.js
+++ src/components/MissionsList.js
@@ -6,14 +6,14 @@ const MissionsList = props => {
       {props.error ? (
         <div className="error">{props.error}</div>
       ) : (
-        <div>
-          {props.missions.map(mission => (
-            <div className="mission" data-testid="mission" key={mission.mission_id}>
-              {mission.mission_name}
-            </div>
-          ))}
-        </div>
-      )}
+          <div>
+            {props.missions.map(mission => (
+              <div className="mission" data-testid="mission" key={mission.mission_id}>
+                {mission.mission_name}:{mission.description}
+              </div>
+            ))}
+          </div>
+        )}
     </section>
   );
 };
