d0f7822f762194cb1e6a73c348fc4134c7a8284f
kirkby <kirkby@neighbor.com>
2020-12-01 19:12:26 -0700

    lecture work-in-progress

--- src/components/MissionsList.test.js
+++ src/components/MissionsList.test.js
@@ -2,6 +2,28 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import MissionsList from './MissionsList';
 
+const missions = [
+  {
+    mission_id: "one",
+    mission_name: "stupid space mission"
+  },
+  {
+    mission_id: "two",
+    mission_name: "stupider space mission"
+  }
+]
+
 test('MissionsList renders', () => {
   render(<MissionsList missions={[]} />);
+})
+
+test("MissionsList shows data when rerendered with new missions data", () => {
+  const { queryAllByTestId, rerender } = render(<MissionsList missions={[]} />)
+
+  expect(queryAllByTestId("mission")).toStrictEqual([]);
+  expect(queryAllByTestId("mission")).toHaveLength(0);
+
+  rerender(<MissionsList missions={missions} />);
+
+  expect(queryAllByTestId("mission")).toHaveLength(2);
 })
