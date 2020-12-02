85c736eee43ec2bffb08490ff5dfced26f5490bd
kirkby <kirkby@neighbor.com>
2020-12-01 21:15:50 -0700

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import App from './App';
 import { fetchMissions as mockFetchMissions } from './api/fetchMissions';
 import { missionsFixture } from './components/MissionsList.test';
 
 // mock the api so we don't make a call and prep to be told what value to return
 jest.mock('./api/fetchMissions');
-console.log(mockFetchMissions);
+// console.log(mockFetchMissions);
 
 test("App renders", () => {
   render(<App />);
@@ -16,5 +16,9 @@ test("App fetches and renders missions data", () => {
   // tell the test system what we want the mission data to return
   mockFetchMissions.mockResolvedValueOnce(missionsFixture);
 
-  render(<App />);
+  const { getByText } = render(<App />);
+  const button = getByText(/get data/i);
+  fireEvent.click(button);
+
+
 })
