63ad3de227cfeeefec4dc8b893d232f36fa550f0
kirkby <kirkby@neighbor.com>
2020-12-01 18:31:07 -0700

    lecture work-in-progress

---
+++ src/components/MissionForm.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import MissionForm from './MissionForm';
+
+test('MissionForm renders', () => {
+  render(<MissionForm />);
+})
