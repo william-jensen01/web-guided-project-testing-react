761b00890f3ac10752fda6420229efe3d92c4801
kirkby <kirkby@neighbor.com>
2020-12-01 19:16:16 -0700

    lecture work-in-progress

---
+++ src/App.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { fetchMissions as mockFetchMissions } from './api/fetchMissions';
+import App from './App';
+
+jest.mock("./api/fetchMissions");
+console.log(mockFetchMissions);
+
+test("App fetches missions data and render data", () => {
+
+}
