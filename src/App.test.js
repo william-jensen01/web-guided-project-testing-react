7255debc676e6702af4d85cb9f24ad193931bf36
kirkby <kirkby@neighbor.com>
2020-12-01 19:22:09 -0700

    texture update

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent, queryAllByAltText, render, wait } from '@testing-library/react';
 import { fetchMissions as mockFetchMissions } from './api/fetchMissions';
 import App from './App';
 
 jest.mock("./api/fetchMissions");
 console.log(mockFetchMissions);
 
-test("App fetches missions data and render data", () => {
+test("App fetches missions data and render data", async () => {
   mockFetchMissions.mockResolvedValueOnce({ data: [{ mission_id: "one", mission_name: "one name" }] });
 
-  const { getByText } = render(<App />)
+  const { getByText, queryAllByTestId } = render(<App />)
 
   const button = getByText(/get data/i);
   fireEvent.click(button);
 
   getByText(/we are fetching data/i);
+  await wait();
+  expect(queryAllByTestId("mission")).toHaveLength(1);
 })
