94a8e53c5f5ebac9f001d08af604ae93a2daf6da
kirkby <kirkby@neighbor.com>
2020-12-01 18:33:37 -0700

    lecture work-in-progress

--- src/components/MissionsList.test.js
+++ src/components/MissionsList.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import MissionsList from 'MissionsList';
+import MissionsList from './MissionsList';
 
 test('MissionsList renders', () => {
   render(<MissionsList />);
